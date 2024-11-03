fd6fe7f824bf519f7d3f5eea0cb7b6050eccabab
Alec Robertson <alec.robertson08@gmail.com>
2024-11-03 00:33:51 -0400

    feat: add fastfetch pkg to home manager

--- module/home-manager.nix
+++ module/home-manager.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
   # add home-manager user settings here
-  home.packages = with pkgs; [git neovim];
+  home.packages = with pkgs; [fastfetch git neovim];
   home.stateVersion = "23.11";
 }
