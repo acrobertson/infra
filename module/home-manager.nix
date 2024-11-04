97dc172562baf6704e0551e88430265c4072bb78
Alec Robertson <alec.robertson08@gmail.com>
2024-11-04 13:03:38 -0500

    feat(nix): enable and configure starship
    Migrated from Homebrew and custom config

--- module/home-manager.nix
+++ module/home-manager.nix
@@ -10,6 +10,43 @@
     };
   };
 
+  programs.starship = {
+    enable = true;
+    settings = {
+      character = {
+        success_symbol = "[⚰](bold green)";
+        error_symbol = "[⚰](bold red)";
+        vimcmd_symbol = "[⚰](bold greend)";
+      };
+      aws = {
+        disabled = true;
+      };
+      deno = {
+        format = "[$symbol($version )]($style)";
+      };
+      docker_context = {
+        format = "[$symbol$context]($style) ";
+      };
+      git_branch = {
+        format = "[$symbol$branch]($style) ";
+        symbol = " ";
+      };
+      nodejs = {
+        format = "[$symbol($version )]($style)";
+      };
+      package = {
+        disabled = true;
+      };
+      php = {
+        format = "[$symbol($version )]($style)";
+        symbol = " ";
+      };
+      rust = {
+        format = "[$symbol($version )]($style)";
+      };
+    };
+  };
+
   programs.zoxide = {
     enable = true;
   };
