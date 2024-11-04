d12bd83e9affb2ef381903b803562a64d2c40e15
Alec Robertson <alec.robertson08@gmail.com>
2024-11-04 12:18:12 -0500

    feat(nix): enable and configure `bat` program
    Migrated from Homebrew. Using the `base16` theme for now to just inherit
    terminal colors. May switch to a custom Tokyonight theme later.

--- module/home-manager.nix
+++ module/home-manager.nix
@@ -3,6 +3,13 @@
   home.packages = with pkgs; [fastfetch fd git htop jq neovim ripgrep sd tldr wget];
   home.stateVersion = "23.11";
 
+  programs.bat = {
+    enable = true;
+    config = {
+      theme = "base16";
+    };
+  };
+
   programs.zoxide = {
     enable = true;
   };
