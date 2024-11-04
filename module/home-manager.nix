35d0881312d8c06206f9fbedc8a930e19490fd53
Alec Robertson <alec.robertson08@gmail.com>
2024-11-04 12:14:30 -0500

    feat(nix): enable zoxide program
    Migrated from homebrew

--- module/home-manager.nix
+++ module/home-manager.nix
@@ -2,4 +2,8 @@
   # add home-manager user settings here
   home.packages = with pkgs; [fastfetch fd git htop jq neovim ripgrep sd tldr wget];
   home.stateVersion = "23.11";
+
+  programs.zoxide = {
+    enable = true;
+  };
 }
