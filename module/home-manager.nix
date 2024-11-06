b8b049e07fdd055d49089b67f0c9c22a5c78566d
Alec Robertson <alec.robertson08@gmail.com>
2024-11-05 23:27:55 -0500

    feat(nix): migrate tmux
    Using separate config file for tmux

--- module/home-manager.nix
+++ module/home-manager.nix
@@ -51,6 +51,28 @@
     };
   };
 
+  programs.tmux = {
+    enable = true;
+    aggressiveResize = true;
+    escapeTime = 0;
+    historyLimit = 5000;
+    mouse = true;
+    plugins = with pkgs; [
+      tmuxPlugins.pain-control
+      tmuxPlugins.resurrect
+      {
+        plugin = tmuxPlugins.continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+        '';
+      }
+    ];
+    # NOTE: Disabling the sensible plugin due to shell issue
+    # see https://github.com/nix-community/home-manager/issues/5952
+    sensibleOnTop = false;
+    extraConfig = builtins.readFile ../config/tmux/tmux.conf;
+  };
+
   programs.zoxide = {
     enable = true;
   };
