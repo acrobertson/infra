149ad37c383fd46aacbd24c8d15a608f65896c81
Alec Robertson <alec.robertson08@gmail.com>
2024-11-05 22:57:27 -0500

    feat(nix): configure zsh program
    Migrated most of my `.zshrc` minus some nvm and pnpm stuff that needs to
    go into project flakes.

--- module/home-manager.nix
+++ module/home-manager.nix
@@ -54,4 +54,30 @@
   programs.zoxide = {
     enable = true;
   };
+
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    enableCompletion = true;
+    oh-my-zsh = {
+      enable = true;
+      plugins = ["git" "macos" "npm" "wp-cli"];
+      theme = "robbyrussell";
+    };
+    sessionVariables = {
+      EDITOR = "vi -e";
+      VISUAL = "nvim";
+    };
+    shellAliases = {
+      # Changed your .gitignore _after_ you have added / committed some files?
+      # run `gri` to untrack anything in your updated .gitignore
+      gri = "git ls-files --ignored --exclude-standard | xargs -0 git rm -r";
+      # Open all merge conflicts or currently changed files in VS Code
+      # TODO: add `nvim` equivalent
+      fix = "git diff --name-only | uniq | xargs code";
+    };
+    syntaxHighlighting = {
+      enable = true;
+    };
+  };
 }
