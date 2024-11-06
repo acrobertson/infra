bd5c773912e4f4665ba4d6d74abc8befed8d7e21
Alec Robertson <alec.robertson08@gmail.com>
2024-11-06 00:03:32 -0500

    feat(fzf): add flexoki fzf theme

--- module/home-manager.nix
+++ module/home-manager.nix
@@ -12,6 +12,23 @@
 
   programs.fzf = {
     enable = true;
+    colors = {
+      fg = "#878580";
+      bg = "#100F0F";
+      hl = "#CECDC3";
+      "fg+" = "#878580";
+      "bg+" = "#1C1B1A";
+      "hl+" = "#CECDC3";
+      border = "#AF3029";
+      header = "#CECDC3";
+      gutter = "#100F0F";
+      spinner = "#24837B";
+      info = "#24837B";
+      separator = "#1C1B1A";
+      pointer = "#AD8301";
+      marker = "#AF3029";
+      prompt = "#AD8301";
+    };
     defaultCommand = "rg --files --hidden --glob '!.git/*'";
     tmux = {
       enableShellIntegration = true;
