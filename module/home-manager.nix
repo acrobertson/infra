54f6a4a8d63fb9c6b521a898b9940dc92f3b6dc6
Alec Robertson <alec.robertson08@gmail.com>
2024-11-16 23:19:26 -0500

    feat(nix): customize nix starship prompt format

--- module/home-manager.nix
+++ module/home-manager.nix
@@ -77,6 +77,9 @@
         format = "[$symbol$branch]($style) ";
         symbol = " ";
       };
+      nix_shell = {
+        format = "[$symbol$state( \($name\))]($style) ";
+      };
       nodejs = {
         format = "[$symbol($version )]($style)";
       };
