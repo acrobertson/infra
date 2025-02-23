b5852c2f7f1b10dd8d8d962d70291e3fad953008
Alec Robertson <alec.robertson08@gmail.com>
2025-02-23 01:20:17 -0500

    chore: update flake
    Change in `nix-darwin` required removing the nix daemon setting.

--- system/darwin.nix
+++ system/darwin.nix
@@ -12,7 +12,6 @@ inputs.darwin.lib.darwinSystem {
   # modules: allows for reusable code
   modules = [
     {
-      services.nix-daemon.enable = true;
       users.users.${username}.home = "/Users/${username}";
     }
     system-config
