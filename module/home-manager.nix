e8a65659a1e20804680930eb971458794b7dc4b8
Alec Robertson <alec.robertson08@gmail.com>
2025-03-16 14:33:59 -0400

    feat: configure git
    Moved gitconfig into Home Manager.

--- module/home-manager.nix
+++ module/home-manager.nix
@@ -8,7 +8,6 @@
     eza
     fastfetch
     fd
-    git
     htop
     jq
     nixfmt-rfc-style
@@ -109,6 +108,83 @@
       prompt = "#AD8301";
     };
     defaultCommand = "rg --files --hidden --glob '!.git/*'";
+  };
+
+  programs.git = {
+    enable = true;
+    signing = {
+      format = "ssh";
+      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIfvNCCpiGWrLTVyUAXulTNPIF7Rda3Y5iynk1dSxfBa";
+      signByDefault = true;
+      # TODO: integrate with GUI from nix?
+      signer = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+    };
+    userEmail = "alec.robertson08@gmail.com";
+    userName = "Alec Robertson";
+    extraConfig = {
+      add = {
+        interactive.useBuiltin = false;
+      };
+      branch = {
+        sort = "-committerdate";
+      };
+      column = {
+        ui = "auto";
+      };
+      commit = {
+        verbose = true;
+      };
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
+      };
+      github = {
+        user = "acrobertson";
+      };
+      help = {
+        autocorrect = "prompt";
+      };
+      init = {
+        defaultBranch = "main";
+      };
+      merge = {
+        conflictstyle = "zdiff3";
+      };
+      pull = {
+        rebase = false;
+      };
+      push = {
+        default = "simple";
+        autoSetupRemote = true;
+        followTags = true;
+      };
+      rebase = {
+        autoSquash = true;
+        autoStash = true;
+        updateRefs = true;
+      };
+      rerere = {
+        enabled = true;
+        autoupdate = true;
+      };
+      tag = {
+        sort = "version:refname";
+      };
+    };
+    delta = {
+      enable = true;
+      options = {
+        navigate = true;
+        line-numbers = true;
+      };
+    };
   };
 
   programs.nushell = {
