495abd01650d92511f1656c811e0a94404a9dab6
Alec Robertson <alec.robertson08@gmail.com>
2025-03-23 23:33:59 -0400

    feat: configure gh cli with home manager
    Ignoring the package since it's already installed with the 1pass shell plugin.

--- module/home-manager.nix
+++ module/home-manager.nix
@@ -118,6 +118,18 @@
     defaultCommand = "rg --files --hidden --glob '!.git/*'";
   };
 
+  programs.gh = {
+    enable = true;
+    # Already installed the package with the 1Pass wrapper.
+    # Only want to use Home Manager to configure it.
+    # This ignores the duplicate package from HM.
+    # see https://github.com/nix-community/home-manager/issues/4763
+    package = pkgs.emptyDirectory;
+    settings = {
+      git_protocol = "ssh";
+    };
+  };
+
   programs.git = {
     enable = true;
     signing = {
