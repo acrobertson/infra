e9a79908ddf280dfd88c62f313bc9a6e3e61229d
Alec Robertson <alec.robertson08@gmail.com>
2026-07-31 23:22:13 -0400

    feat(ai): customize claude-code statusline, default model, etc.

--- modules/home-manager/common/default.nix
+++ modules/home-manager/common/default.nix
@@ -58,6 +58,7 @@
     with pkgs;
     [
       btop
+      unstable.ccusage
       curl
       eza
       fastfetch
--- modules/home-manager/programs/claude-code/default.nix
+++ modules/home-manager/programs/claude-code/default.nix
@@ -19,6 +19,34 @@ in
     ];
 
     settings = {
+      autoCompactEnabled = false;
+      editorMode = "vim";
+      effortLevel = "medium";
+      model = "claude-opus-4-8";
+      permissions = {
+        allow = [
+          "Read(./.env.example)"
+        ];
+        deny = [
+          "Read(./.env)"
+          "Read(./.env.*)"
+          "Read(./secrets/**)"
+          "Read(./config/credentials.json)"
+          "Read(./build)"
+          "Read(./dist)"
+        ];
+        disableBypassPermissionsMode = "disable";
+      };
+      spinnerTipsEnabled = false;
+      spinnerVerbs = {
+        mode = "replace";
+        verbs = [ "Trying" ];
+      };
+      statusLine = {
+        command = "ccusage statusline --context-low-threshold 20 --context-medium-threshold 50";
+        padding = 0;
+        type = "command";
+      };
       terminalProgressBarEnabled = true;
       theme = "auto";
       tui = "fullscreen";
